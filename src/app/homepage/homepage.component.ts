4b3d2bd8be3c7597d43c8f4d0da07d790d9b0f33
itzikalgrisi <itzik@uniper-care.com>
2019-09-02 15:18:25 +0300

    Change text

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -11,22 +11,15 @@ import { Overlay } from '@angular/cdk/overlay';
 export class HomepageComponent implements OnInit, AfterViewInit {
 
   imageUrlArray = [
-    // {url: '/assets/images/happy.jpg', text: 'Maximize the value of your asset, you <span class="bold">deserve it</span>',
-    //  btnText: 'Learn More'},
-    // {url: '/assets/images/happy_9.jpg', text: 'Life insurance is valuable, we want you to <span class="bold">keep it!</span>',
-    //   btnText: 'Get Policy Forecast', goto: '/clients/report'},
-    // {url: '/assets/images/investing.png', text: 'Investing In <span class="bold">' +
-    //   'Life Settlements</span><br /><span class="bold">Made Simple</span>, Secure & Liquid',
-    //   btnText: 'Go To Market', goto: '/aws'}
-    {url: '/assets/images/img1.jpg', text: 'Maximize the value of your asset, you <span class="bold">deserve it</span>',
+    {url: '/assets/images/img1.jpg', text: 'Maximize the value <br />of your asset,<br />you <span class="bold">deserve it</span>',
      btnText: 'Learn More'},
-    {url: '/assets/images/img2.jpg', text: 'Life insurance is valuable, we want you to <span class="bold">keep it!</span>',
+    {url: '/assets/images/img2.jpg', text: 'Life insurance is valuable,<br /> we want you to <span class="bold">keep it!</span>',
       btnText: 'Get Policy Forecast', goto: '/clients/report'},
     {url: '/assets/images/img3.jpg', text: 'Investing In <span class="bold">' +
-      'Life Settlements</span><br /><span class="bold">Made Simple</span>, Secure & Liquid',
-      btnText: 'Go To Market', goto: '/aws'}
-
+      'Life Settlements</span><br /><span class="bold">Made Simple</span>, <br />Secure & Liquid',
+      btnText: 'Go To Market', goto: '/aws'},
     ];
+
   position = 0;
   amount;
   prev: HTMLElement;
