94307c07482c4d3959c673bff9d09fea2ccfadd7
itzikalgrisi <itzik@uniper-care.com>
2019-09-23 11:02:02 +0300

    mobile

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -9,7 +9,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class RegistrationComponent implements OnInit {
 
-  investorView = false;
+  investorView = true;
   policyHolderView = false;
   firstName: String;
   lastName: String;
