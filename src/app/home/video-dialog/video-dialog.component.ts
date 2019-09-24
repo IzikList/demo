7309df727c0480aa8550d392674e02381a84f8d1
itzikalgrisi <itzik@uniper-care.com>
2019-09-24 22:33:22 +0300

    obile

--- src/app/home/video-dialog/video-dialog.component.ts
+++ src/app/home/video-dialog/video-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-video-dialog',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<VideoDialogComponent>) { }
 
   ngOnInit() {
   }
 
+  closePopup() {
+    this.dialogRef.close();
+  }
 }
